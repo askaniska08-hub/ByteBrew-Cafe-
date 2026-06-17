1b0030c0ded46d58351fe074d18fc3e9b52373b9
A.S. Kaniska <askaniska08@gmail.com>
2026-06-18 01:14:50 +0530

    Enhance billing script with menu and calculations
    Added menu items and calculation for final bill with discount and tax.

--- ByteBrew café Bill.py
+++ ByteBrew café Bill.py
@@ -1,1 +1,23 @@
-print("Welcome to ByteBrew café ")
+print("Welcome to ByteBrew café !")
+
+Menu_1 = ("Latte") 
+Menu_2 = ("Cappuccino")
+Menu_3 = ("Mocha") 
+Menu_4= ("Macchiato")
+order = input("ENTER ITEM NAME :")
+
+quantity = int(input("ENTER QUANTITY :"))
+amount = float(input("ENTER PRICE PER ITEM :"))
+discount = input("APPLY DISCOUNT? (True/False) : True")
+
+n = 120
+print("-" * n)
+
+subtotal =(quantity * amount)
+print("SUBTOTAL :", subtotal)
+discount = print("DISCOUNT APPLIED : 10%")
+tax = print("TAX : 5%")
+
+DISCOUNT = (subtotal - (10/100 * subtotal))
+TAX = (DISCOUNT + (DISCOUNT * 5/100))
+print("FINAL BILL :", TAX)
