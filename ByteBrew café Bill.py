5fda2daa985d4c555ae806b232954dc2e1bf4bbe
A.S. Kaniska <askaniska08@gmail.com>
2026-06-19 16:21:43 +0530

    More user friendly
    added space between two lines (\n)
    created underlining effects
    used if else condition
    used center alignment code

--- ByteBrew café Bill.py
+++ ByteBrew café Bill.py
@@ -1,23 +1,51 @@
 print("Welcome to ByteBrew café !")
+n=120
+print("-"*n)
 
-Menu_1 = ("Latte") 
-Menu_2 = ("Cappuccino")
-Menu_3 = ("Mocha") 
-Menu_4= ("Macchiato")
+print("MENU \n")
+
+print("Latte      - Rs 150")
+print("Cappuccino - Rs 100")
+print("Mocha      - Rs 120")
+print("Macchiato  - Rs 200")
+
+print("-"*n)
+
+Menu = ["Latte" , "Cappuccino" , "Mocha" , "Macchiato" ]
 order = input("ENTER ITEM NAME :")
+if order in Menu:
+        print("Order accepted")
+else: exit(print("Sorry, Item not available"))
 
 quantity = int(input("ENTER QUANTITY :"))
 amount = float(input("ENTER PRICE PER ITEM :"))
-discount = input("APPLY DISCOUNT? (True/False) : True")
+apply_discount = (input("APPLY DISCOUNT? (True/False) :"))
 
-n = 120
+
 print("-" * n)
 
+print("BILL SUMMARY")
+print("~"*12)
 subtotal =(quantity * amount)
-print("SUBTOTAL :", subtotal)
-discount = print("DISCOUNT APPLIED : 10%")
-tax = print("TAX : 5%")
+print("\nSUBTOTAL :", subtotal)
 
-DISCOUNT = (subtotal - (10/100 * subtotal))
-TAX = (DISCOUNT + (DISCOUNT * 5/100))
-print("FINAL BILL :", TAX)
+if apply_discount=="True":
+        print("DISCOUNT APPLIED : 10%")
+        tax = print("TAX : 5%")
+
+        DISCOUNT = (subtotal - (10/100 * subtotal))
+        TAX = (DISCOUNT + (DISCOUNT * 5/100))
+
+
+else:
+        print("NO DISCOUNT APPLIED")
+        print("TAX : 5%")
+
+        TAX = (subtotal + (subtotal * 5/100))
+
+print("\nFINAL BILL : ", TAX )
+
+
+print("-"*n)
+print("Thank You, Visit Again".center(n))
+print("-"*n)
